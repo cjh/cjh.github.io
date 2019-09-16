fef6ff3b81702a5a60a65e471204cf42158b7c09
Chris H <chjohasbrouck@gmail.com>
2019-09-16 08:26:21 -0700

    Made some generative art (Geometric Dandelions)

---
+++ dandelion.js
@@ -0,0 +1,156 @@
+const CANVAS1 = document.getElementById('dandy-1');
+const CANVAS2 = document.getElementById('dandy-2');
+const CANVAS3 = document.getElementById('dandy-3');
+const CTX1 = CANVAS1.getContext('2d');
+const CTX2 = CANVAS2.getContext('2d');
+const CTX3 = CANVAS3.getContext('2d');
+const NUM_COLORS = 5;
+const NUM_DANDELIONS = 32;
+
+let WIDTH, HEIGHT;
+let colors, maxSize, pilotx, piloty, startTime, maxDelay = 0, frames = [];
+CTX1.globalCompositeOperation = 'multiply';
+CTX2.globalCompositeOperation = 'multiply';
+CTX3.globalCompositeOperation = 'multiply';
+
+function init() {
+    function revert() {
+        if (frames.length > 0) {
+            let frame = frames.pop();
+            frame.ctx.putImageData(frame.img, 0, 0);
+            setTimeout(function () {
+                revert();
+            }, 10);
+        } else {
+            init();
+        }
+    }
+    if (frames.length > 0) {
+        revert();
+        return;
+    }
+    WIDTH = window.innerWidth;
+    HEIGHT = window.innerHeight;
+    CANVAS1.width = WIDTH;
+    CANVAS1.height = HEIGHT;
+    CANVAS2.width = WIDTH;
+    CANVAS2.height = HEIGHT;
+    CANVAS3.width = WIDTH;
+    CANVAS3.height = HEIGHT;
+    maxDelay = 0;
+    maxSize = Math.min(WIDTH, HEIGHT) / 6;
+    colors = generateRandomColors(NUM_COLORS);
+    clearAll();
+    dandelions = [];
+    freshDandelion('#fa3f32', 1300);
+    pilotx = random(maxSize / 2, WIDTH - maxSize / 2);
+    piloty = random(maxSize / 2, HEIGHT - maxSize / 2);
+    dandelions[0].x = pilotx;
+    dandelions[0].y = piloty;
+
+    while (dandelions.length < NUM_DANDELIONS) {
+        freshDandelion();
+    }
+    startTime = new Date();
+    requestAnimationFrame(draw);
+}
+
+function drawDandy(d, context, x, y) {
+    //context.filter = 'blur(' + d.blurFactor.toString() + 'px)';
+    let rgb = hexToRgb(d.color)
+    rgb.push(1);
+    rgb = 'rgba(' + rgb.join(',') + ')';
+    console.log(rgb);
+    context.strokeStyle = rgb;
+    context.shadowBlur = 5;
+    context.shadowColor = d.color;
+    context.beginPath();
+    for (i = 0; i < d.path.length; i++) {
+        context.lineTo(x + d.path[i][0], y + d.path[i][1]);
+    }
+    context.moveTo(x, y);
+    context.lineTo(x, HEIGHT * 2);
+    context.stroke();
+    context.closePath();
+}
+
+let dandelions = [];
+
+function freshDandelion(color = null, delay = null) {
+    if (color == null) {
+        color = colors[random(0, NUM_COLORS - 1)];
+    }
+    if (delay == null) {
+        delay = random(200, 2400);
+    }
+    x = random(0, WIDTH);
+    y = random(0, HEIGHT);
+    if (pilotx != null && piloty != null) {
+        while (Math.abs(x - pilotx) < maxSize && Math.abs(y - piloty) < maxSize) {
+            x = random(0, WIDTH);
+            y = random(0, HEIGHT);
+        }
+    }
+
+    let width = maxSize * 3, height = HEIGHT * 2;
+    dandelion = {
+        path: [],
+        color: color,
+        blurFactor: [1, 1, 1, 2, 2, 3, 3, 3][random(0, 7)],
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+        cache: document.createElement('canvas'),
+        delay: delay,
+        ctx: null
+    }
+    dandelion.ctx = dandelion.blurFactor == 1 ?
+        CTX3 : dandelion.blurFactor == 2 ? CTX2 : CTX1;
+    maxDelay = Math.max(maxDelay, dandelion.delay);
+    let context = dandelion.cache.getContext('2d');
+    let size = Math.min(WIDTH, HEIGHT);
+    size = random(size / 24, size / 6);
+    dandelion.cache.width = width;
+    dandelion.cache.height = height;
+    for (i = 0; i < 200; i++) {
+        let r = size * Math.PI * Math.random(),
+            h = Math.random() + Math.random(),
+            d = h > 1 ? size - h : h,
+            s = d * Math.cos(r),
+            m = d * Math.sin(r);
+        dandelion.path.push([s, m]);
+    }
+    drawDandy(dandelion, context, maxSize * 1.5, maxSize * 1.5);
+    dandelions.push(dandelion);
+}
+
+function draw() {
+    clearAll();
+    let time = new Date();
+
+    let ms = time.getTime() - startTime.getTime();
+    let y = HEIGHT - ms / 10 * (HEIGHT / 100) + 250;
+    let maxSize = Math.min(WIDTH, HEIGHT) / 6;
+    let offx = -(maxSize * 1.5);
+    let offy = -(maxSize * 1.5);
+    for (var i = 0; i < dandelions.length; i++) {
+        let d = dandelions[i];
+        setTimeout(function () {
+            d.ctx.drawImage(d.cache, d.x + offx, d.y + offy);
+            let frame = {
+                ctx: d.ctx,
+                img: d.ctx.getImageData(0, 0, WIDTH, HEIGHT)
+            };
+            frames.push(frame);
+        }, (i * i) / 4 * 10);
+    }
+}
+
+function clearAll() {
+    CTX1.clearRect(0, 0, WIDTH, HEIGHT);
+    CTX2.clearRect(0, 0, WIDTH, HEIGHT);
+    CTX3.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+init();
---
+++ utils.js
@@ -0,0 +1,30 @@
+function random(t, o) {
+    var a = o + 1 - t,
+        n = Math.random() * a + t;
+    return Math.floor(n)
+}
+var generateRandomColors = (t, o = "lab", a = .175, n = 4) => {
+    let e = [];
+    const i = Math.floor(t / n),
+        r = t % n,
+        h = random(0, 360),
+        d = [0, 60, 120, 180, 240, 300].map(t => (h + t) % 360),
+        s = random(5, 40),
+        m = random(0, 20),
+        l = 90 - m;
+    e.push(HUSL.toHex(d[0], s, m * random(.25, .75)));
+    for (let t = 0; t < i - 1; t++) e.push(HUSL.toHex(d[0], s, m + l * Math.pow(t / (i - 1), 1.5)));
+    const w = random(50, 70),
+        c = w + 30,
+        g = random(45, 80),
+        u = Math.min(g + 40, 95);
+    for (let t = 0; t < i + r - 1; t++) e.push(HUSL.toHex(d[random(0, d.length - 1)], random(w, c), random(g, u)));
+    return e.push(HUSL.toHex(d[0], s, l)), chroma.scale(e).padding(a).mode(o).colors(t)
+},
+    init,
+    requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+const hexToRgb = hex =>
+  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+    .substring(1).match(/.{2}/g)
+    .map(x => parseInt(x, 16))
